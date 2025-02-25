b81cfc91d9df2219f28bd67bc2e5b0c03b2c301e
Aliya <aakhisamova@gmail.com>
2025-02-25 16:02:03 +0400

    first step of application Task Manager

---
+++ app/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("io.freefair.lombok") version "8.6"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("net.datafaker:datafaker:2.0.1")
+    implementation("org.instancio:instancio-junit:3.3.0")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ app/src/main/java/hexlet/code/AppApplication.java
@@ -0,0 +1,18 @@
+package hexlet.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+    public static String getDatabaseUrl() {
+        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+    }
+}
